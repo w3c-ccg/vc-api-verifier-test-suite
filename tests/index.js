6b3ec79b29c06c93eadf0d93cc26c5f9f3fd8bf0
Tashi D Gyeltshen <tashidgyeltshen@hotmail.com>
2022-03-22 13:45:16 -0400

    Add remaining tests.

--- tests/index.js
+++ tests/index.js
@@ -146,5 +146,178 @@ for(const verifier of testAPIs) {
       should.not.exist(response);
       err.status.should.equal(400);
     });
+    it('MUST not verify if "@context" is not an array.', async function() {
+      this.test.cell = {
+        columnId: verifier.name,
+        rowId: this.test.title
+      };
+      const implementation = new Implementation(verifier);
+      const copyVC = {...validVC};
+      const invalidContextTypes = ['string', {}, null, undefined, 10, true];
+      for(const invalidContextType of invalidContextTypes) {
+        copyVC['@context'] = invalidContextType;
+        let response;
+        let err;
+        try {
+          response = await implementation.verify({
+            credential: copyVC
+          });
+        } catch(e) {
+          err = e;
+        }
+        should.exist(err);
+        should.not.exist(response);
+        err.status.should.equal(400);
+      }
+    });
+    it('MUST not verify if "@context" items are not strings.',
+      async function() {
+        this.test.cell = {
+          columnId: verifier.name,
+          rowId: this.test.title
+        };
+        const implementation = new Implementation(verifier);
+        const copyVC = {...validVC};
+        const invalidContextItemTypes = [[], {}, null, undefined, 10, true];
+        for(const invalidContextItemType of invalidContextItemTypes) {
+          copyVC['@context'] = [invalidContextItemType];
+          let response;
+          let err;
+          try {
+            response = await implementation.verify({
+              credential: copyVC
+            });
+          } catch(e) {
+            err = e;
+          }
+          should.exist(err);
+          should.not.exist(response);
+          err.status.should.equal(400);
+        }
+      });
+    it('MUST not verify if "type" is not an array.', async function() {
+      this.test.cell = {
+        columnId: verifier.name,
+        rowId: this.test.title
+      };
+      const implementation = new Implementation(verifier);
+      const copyVC = {...validVC};
+      const invalidTypes = ['string', {}, null, undefined, 10, true];
+      for(const invalidType of invalidTypes) {
+        copyVC.type = invalidType;
+        let response;
+        let err;
+        try {
+          response = await implementation.verify({
+            credential: copyVC
+          });
+        } catch(e) {
+          err = e;
+        }
+        should.exist(err);
+        should.not.exist(response);
+        err.status.should.equal(400);
+      }
+    });
+    it('MUST not verify if "type" items are not strings.', async function() {
+      this.test.cell = {
+        columnId: verifier.name,
+        rowId: this.test.title
+      };
+      const implementation = new Implementation(verifier);
+      const copyVC = {...validVC};
+      const invalidTypeItemTypes = [[], {}, null, undefined, 10, true];
+      for(const invalidItemType of invalidTypeItemTypes) {
+        copyVC.type = [invalidItemType];
+        let response;
+        let err;
+        try {
+          response = await implementation.verify({
+            credential: copyVC
+          });
+        } catch(e) {
+          err = e;
+        }
+        should.exist(err);
+        should.not.exist(response);
+        err.status.should.equal(400);
+      }
+    });
+    it('MUST not verify  if "issuer" is not an object or a string.',
+      async function() {
+        this.test.cell = {
+          columnId: verifier.name,
+          rowId: this.test.title
+        };
+        const implementation = new Implementation(verifier);
+        const copyVC = {...validVC};
+        const invalidIssuerTypes = [[], null, undefined, 10, true];
+        for(const invalidIssuerType of invalidIssuerTypes) {
+          copyVC.issuer = invalidIssuerType;
+          let response;
+          let err;
+          try {
+            response = await implementation.verify({
+              credential: copyVC
+            });
+          } catch(e) {
+            err = e;
+          }
+          should.exist(err);
+          should.not.exist(response);
+          err.status.should.equal(400);
+        }
+      });
+    it('MUST not verify if "credentialSubject" is not an object.',
+      async function() {
+        this.test.cell = {
+          columnId: verifier.name,
+          rowId: this.test.title
+        };
+        const implementation = new Implementation(verifier);
+        const copyVC = {...validVC};
+        const invalidCredentialSubjectTypes = [
+          'string', null, undefined, 10, true, []
+        ];
+        for(const invalidType of invalidCredentialSubjectTypes) {
+          copyVC.credentialSubject = invalidType;
+          let response;
+          let err;
+          try {
+            response = await implementation.verify({
+              credential: copyVC
+            });
+          } catch(e) {
+            err = e;
+          }
+          should.exist(err);
+          should.not.exist(response);
+          err.status.should.equal(400);
+        }
+      });
+    it('MUST not verify if "proof" is not an object.', async function() {
+      this.test.cell = {
+        columnId: verifier.name,
+        rowId: this.test.title
+      };
+      const implementation = new Implementation(verifier);
+      const copyVC = {...validVC};
+      const invalidProofTypes = ['string', null, undefined, 10, true, []];
+      for(const invalidProofType of invalidProofTypes) {
+        copyVC.proof = invalidProofType;
+        let response;
+        let err;
+        try {
+          response = await implementation.verify({
+            credential: copyVC
+          });
+        } catch(e) {
+          err = e;
+        }
+        should.exist(err);
+        should.not.exist(response);
+        err.status.should.equal(400);
+      }
+    });
   });
 }
