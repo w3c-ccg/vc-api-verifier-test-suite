3c39639af7f9a2a3d3e672b9812f28cfe06e75ee
Tashi D Gyeltshen <tashidgyeltshen@hotmail.com>
2022-03-25 14:07:01 -0400

    Deep clone validVC, update secret name, and add helpers file.

--- tests/implementation.js
+++ tests/implementation.js
@@ -3,13 +3,10 @@
  */
 'use strict';
 
-const {decodeSecretKeySeed} = require('bnid');
-const didKey = require('@digitalbazaar/did-method-key');
-const {Ed25519Signature2020} = require('@digitalbazaar/ed25519-signature-2020');
 const https = require('https');
-const {ZcapClient} = require('@digitalbazaar/ezcap');
+const {httpClient} = require('@digitalbazaar/http-client');
+const {getZcapClient} = require('./helpers');
 
-const didKeyDriver = didKey.driver();
 const agent = new https.Agent({rejectUnauthorized: false});
 
 const _headers = {
@@ -36,17 +33,22 @@ class Implementation {
       if(auth && auth.type === 'oauth2-bearer-token') {
         headers.Authorization = `Bearer ${auth.accessToken}`;
       }
-      let capability;
+      let result;
       if(this.settings.verifier.zcap) {
-        capability = JSON.parse(this.settings.verifier.zcap);
+        const capability = JSON.parse(this.settings.verifier.zcap);
+        const zcapClient = await getZcapClient();
+        result = await zcapClient.write({
+          url: this.settings.verifier.endpoint,
+          headers,
+          capability,
+          json: body
+        });
+      } else {
+        result = await httpClient.post(
+          this.settings.verifier.endpoint,
+          {headers, agent, json: body}
+        );
       }
-      const zcapClient = await _getZcapClient();
-      const result = await zcapClient.write({
-        url: this.settings.verifier.endpoint,
-        headers,
-        capability,
-        json: body
-      });
       return result;
     } catch(e) {
       // this is just to make debugging easier
@@ -56,22 +58,6 @@ class Implementation {
       throw e;
     }
   }
-}
-
-async function _getZcapClient() {
-  if(!process.env.CLIENT_SECRET) {
-    throw new Error('ENV variable CLIENT_SECRET is required.');
-  }
-  const secretKeySeed = process.env.CLIENT_SECRET;
-  const seed = await decodeSecretKeySeed({secretKeySeed});
-  const didKey = await didKeyDriver.generate({seed});
-  const {didDocument: {capabilityInvocation}} = didKey;
-  const zcapClient = new ZcapClient({
-    SuiteClass: Ed25519Signature2020,
-    invocationSigner: didKey.keyPairs.get(capabilityInvocation[0]).signer(),
-    agent
-  });
-  return zcapClient;
 }
 
 module.exports = Implementation;
