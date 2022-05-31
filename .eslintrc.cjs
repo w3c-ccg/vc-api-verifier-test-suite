79d7adc0ae7c7a6f209182ca72352ae714af20ff
Tashi D Gyeltshen <tashidgyeltshen@hotmail.com>
2022-05-31 10:39:58 -0400

    Add `eslint-plugin-unicorn` to dev deps.

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -5,8 +5,13 @@ module.exports = {
   },
   extends: [
     'eslint-config-digitalbazaar',
-    'eslint-config-digitalbazaar/jsdoc'
+    'eslint-config-digitalbazaar/jsdoc',
+    'digitalbazaar/module',
   ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module'
+  },
   rules: {
     'jsdoc/check-examples': 0
   }
--- tests/.eslintrc.cjs
+++ tests/.eslintrc.cjs
@@ -2,14 +2,5 @@ module.exports = {
   env: {
     node: true,
     mocha: true
-  },
-  extends: [
-    'eslint-config-digitalbazaar',
-    'eslint-config-digitalbazaar/jsdoc',
-    'digitalbazaar/module',
-  ],
-  parserOptions: {
-    ecmaVersion: 'latest',
-    sourceType: 'module'
   }
 };
