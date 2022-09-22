18c2a8c2f86871c74942681a136afc9e8136cb57
Andrew Jones <ajones@digitalbazaar.com>
2022-09-22 11:35:14 -0400

    Lint use single qoutes in mock-data.

--- mock-data/vc.js
+++ mock-data/vc.js
@@ -1,10 +1,10 @@
 export const vc = {
-  "@context": [
-    "https://www.w3.org/2018/credentials/v1",
-    "https://w3id.org/security/suites/ed25519-2020/v1"
+  '@context': [
+    'https://www.w3.org/2018/credentials/v1',
+    'https://w3id.org/security/suites/ed25519-2020/v1'
   ],
-  "type": ["VerifiableCredential"],
-  "credentialSubject": {
-    "id": "did:key:z6MkhTNL7i2etLerDK8Acz5t528giE5KA4p75T6ka1E1D74r"
+  type: ['VerifiableCredential'],
+  credentialSubject: {
+    id: 'did:key:z6MkhTNL7i2etLerDK8Acz5t528giE5KA4p75T6ka1E1D74r'
   }
 };
